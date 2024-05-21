0c9ed2e70bdbb680a400ae5c240076ca41a91d4f
aa <102574887+copypastin@users.noreply.github.com>
2024-05-21 16:16:03 +0000

    oopsie poopsie

--- index.js
+++ index.js
@@ -2,7 +2,15 @@
 
 function generateBets (numBets) {
     let bets = []
-    for (let i = 0; i < numBets; i++)  bets.push(Math.floor(Math.random()* 6 + 1));
+    for (let i = 0; i < numBets; i++) {
+        while(true){
+            let bet = Math.floor(Math.random()*6 + 1);
+            if(!bets.includes(bet)) {
+                bets.push(bet);
+                break;
+            }
+        }
+    } 
     return bets;
 }
 
@@ -10,7 +18,7 @@ const simulate = (numBets, testNum) => {
     // const testNum = 1000000
     // const numBets = 1; 
     const startingMoney = 1000;
-    const betAmount = 10;
+    const betAmount = 2;
     let economy = 0;
 
     let winrate = {
@@ -26,10 +34,10 @@ const simulate = (numBets, testNum) => {
         let money = startingMoney;
         let dices = [Math.floor(Math.random()* 6 + 1), Math.floor(Math.random()* 6 + 1), Math.floor(Math.random()* 6 + 1)];
         let bets = generateBets(numBets);
-        money -= betAmount*bets.length;
+        // console.log(bets)
+        // console.log(dices)
     
-        for(let j = 0; j < bets.length; j++) {
-    
+        for(let j = 0; j < bets.length; j++) {    
             if(dices[0] == dices[1] && dices[0] == dices[2]) {
                 if(dices.includes(bets[j])) {
                     money += betAmount*4;
@@ -46,7 +54,7 @@ const simulate = (numBets, testNum) => {
                     winrate["2x"]++;
                 }
                 else if(dices.includes(bets[j])) {
-                    money -= betAmount*0.5;
+                    money += betAmount*0.5;
                     winrate["0.5x"]++;
                 }
                 else {
@@ -54,9 +62,9 @@ const simulate = (numBets, testNum) => {
                     winrate["no win"]++;
                 }
             }
-            else if(dices[0] != bets[1] && dices[1] != bets[2] && dices[2] != bets[0]) {
+            else if(dices[0] != dices[1] && dices[1] != dices[2] && dices[2] != dices[0]) {
                 if(dices.includes(bets[j])) {
-                    money -= betAmount*0.5;
+                    money += betAmount*0.5;
                     winrate["0.5x"]++;
                 } 
                 else {
@@ -67,6 +75,7 @@ const simulate = (numBets, testNum) => {
             else {
                 money -= betAmount;
                 winrate["no win"]++;
+                console.log(`error`)
             }
         }
     
@@ -79,14 +88,13 @@ const simulate = (numBets, testNum) => {
     console.log(`2x Wins: ${winrate["2x"]} (${(winrate["2x"]/winrate["total"]*100).toFixed(2)}%)`);
     console.log(`4x Wins: ${winrate["4x"]} (${(winrate["4x"]/winrate["total"]*100).toFixed(2)}%)`);
     console.log(`No Wins: ${winrate["no win"]} (${(winrate["no win"]/winrate["total"]*100).toFixed(2)}%)`);
-    console.log(`AVERAGE AMOUNT WON : ${(economy - startingMoney*testNum) /testNum}`);
+    console.log(`AVERAGE AMOUNT WON : $${(economy - startingMoney*testNum) /testNum}`);
     console.log(`Total Bets Placed: ${winrate["total"]}`);
         
 }
 
-simulate(1, 10000000)
-simulate(2, 10000000)
-simulate(3, 10000000)
-simulate(4, 10000000)
-simulate(5, 10000000)
-simulate(6, 10000000)
+simulate(1, 100000)
+simulate(2, 100000)
+simulate(3, 100000)
+simulate(4, 100000)
+simulate(5, 100000)
